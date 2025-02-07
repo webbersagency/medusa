acc1e8e262ded42173f699d45631d77501bd5b85
Shahed Nasser <shahednasser@gmail.com>
2025-02-07 12:41:19 +0200

    docs: update intro of custom item price (#11358)
    * docs: update intro of custom item price guide
    
    * small change

--- www/apps/resources/generated/edit-dates.mjs
+++ www/apps/resources/generated/edit-dates.mjs
@@ -5904,5 +5904,5 @@ export const generatedEditDates = {
   "references/core_flows/Order/Steps_Order/variables/core_flows.Order.Steps_Order.updateOrderChangesStepId/page.mdx": "2025-01-27T11:43:49.278Z",
   "app/troubleshooting/test-errors/page.mdx": "2025-01-31T13:08:42.639Z",
   "app/commerce-modules/product/variant-inventory/page.mdx": "2025-02-03T12:19:45.706Z",
-  "app/examples/guides/custom-item-price/page.mdx": "2025-02-06T10:37:57.282Z"
+  "app/examples/guides/custom-item-price/page.mdx": "2025-02-07T09:21:11.170Z"
 }
